27042eb4452c5f89aeb719d884b7ea3fa6ed5dab
LovekeshAnand <lovekeshanand6@gmail.com>
2025-04-05 22:14:53 +0530

    rate-increase

--- api/src/middleware/rateLimiter.js
+++ api/src/middleware/rateLimiter.js
@@ -2,14 +2,14 @@ import rateLimit from "express-rate-limit";
 
 const registerLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10, // Limit each IP to 5 requests per windowMs
+  max: 20, // Limit each IP to 5 requests per windowMs
   message: {
     success: false,
     message: "Too many requests. Please try again later.",
   },
   headers: true, // Include default rate limit headers
   handler: (req, res, next) => {
-    res.set("X-RateLimit-Limit", 10); // Set max limit header
+    res.set("X-RateLimit-Limit", 20); // Set max limit header
     res.set("X-RateLimit-Remaining", 0); // Always 0 when limit is exceeded
     res.set("X-RateLimit-Reset", new Date(Date.now() + 15 * 60 * 1000).toISOString()); // Reset time
     res.status(429).json({
