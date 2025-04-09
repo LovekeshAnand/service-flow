64a62931e1484210498b29d23026ac4d0cac09e2
LovekeshAnand <lovekeshanand6@gmail.com>
2025-04-09 17:09:23 +0530

    scroll-update

--- client/src/components/LenisProvider.jsx
+++ client/src/components/LenisProvider.jsx
@@ -1,31 +1,4 @@
 'use client';
-<<<<<<< HEAD
- 
- import { useEffect } from 'react';
- import Lenis from '@studio-freight/lenis';
- 
- export default function LenisProvider({ children }) {
-   useEffect(() => {
-     const lenis = new Lenis({
-       duration: 1.2,
-       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-     });
- 
-     function raf(time) {
-       lenis.raf(time);
-       requestAnimationFrame(raf);
-     }
- 
-     requestAnimationFrame(raf);
- 
-     return () => {
-       lenis.destroy();
-     };
-   }, []);
- 
-   return <>{children}</>;
- }
-=======
 
 import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
@@ -51,4 +24,3 @@ export default function LenisProvider({ children }) {
 
   return <>{children}</>;
 }
->>>>>>> 31fae96cfba8fd54524ba8d74dabd5eec25d6b96
