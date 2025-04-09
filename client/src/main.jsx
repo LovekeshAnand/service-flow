31fae96cfba8fd54524ba8d74dabd5eec25d6b96
Alex1Shelby <ydrgaming71@gmail.com>
2025-04-09 16:55:42 +0530

    SmoothScroll

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { AlertProvider } from "./components/AlertProvider"; // Import AlertProvider
+import LenisProvider from "./components/LenisProvider";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <LenisProvider>
     <AlertProvider> {/* ✅ Wrap App with AlertProvider */}
       <App />
     </AlertProvider>
+    </LenisProvider>
   </StrictMode>
 );
